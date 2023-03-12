e3f32d3889c94de570b08cca955646678a2810e8
gabriel <ggonzalez@auction.com>
2023-03-12 01:09:55 -0500

    First commit

---
+++ index.d.ts
@@ -0,0 +1,39 @@
+interface Ivent {
+    id: string,
+    name: string,
+    description: string,
+    starting_hour: Date,
+    ending_hour: Date,
+    address: String,
+    latitude: number,
+    longitude: number,
+    cover: CloudStorageImage,
+    main_category: number,
+    categories: number[],
+    virtual: boolean,
+    min_age: number,
+    max_age: number,
+    capacity: number,
+    owner_id: string,
+    owner_name: string,
+    localities: Locality[],
+    starting_price: number,
+    event_date: Date,
+    followers: string[],
+    short_address: string,
+    images: CloudStorageImage[]
+}
+
+interface Locality {
+    id: number,
+    name: string,
+    price: number,
+    description?: string | null,
+}
+
+interface CloudStorageImage {
+    url : string,
+    file_name : string
+}
+
+export {Ivent, Locality, CloudStorageImage}
